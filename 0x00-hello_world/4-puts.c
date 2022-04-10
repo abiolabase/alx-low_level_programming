7e6e19cf7ce9d29816f09142ce5b068cffa08e31
Abiola Ajibowu <talk2sherifabiola@yahoo.com>
2022-04-10 05:05:16 -0700

    Use betty coding style and documentation

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
-int main() {
+int main(void)
+{
   /* This program prints string to stdout*/
-  
   char mystr[] = "\"Programming is like building a multilingual puzzle";
   puts(mystr);
   return (0);
