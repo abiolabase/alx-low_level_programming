6cfdf16f02a3e9c2678c3ff0b34e2f09fbca6e35
Abiola Ajibowu <talk2sherifabiola@yahoo.com>
2022-04-10 06:56:43 -0700

    Print the size of data type

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the size of various data type on computer
+ *
+ * Return: 0
+ */
+int main(void)
+{
+char data_type[] = {"char", "int", "long int", "long long int", "float"};
+for (i=0; i<5; i++)
+{
+if (data_type[i] == "int")
+{
+printf("Size of an %s: %d byte(s)\n", data_type[i], sizeof(data_type[i]);
+}
+else
+{
+printf("Size of a %s: %d byte(s)\n", data_type[i], sizeof(data_type[i]);
+}
+return (0);
+}
