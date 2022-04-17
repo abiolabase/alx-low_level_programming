4446c054733bd75f9188f65946a243ea40982666
Abiola Ajibowu <talk2sherifabiola@yahoo.com>
2022-04-17 14:44:23 -0700

    Print alphabets in small then capital letter

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabet to display
+ *
+ * Return: 0
+ */
+int main(void)
+{
+char c, C;
+c = 'a';
+C = 'A';
+while (c <= 'z')
+{
+putchar(c);
+c++;
+}
+while (C <= 'Z')
+{
+putchar(C);
+C++;
+}
+putchar('\n');
+return (0);
+}
