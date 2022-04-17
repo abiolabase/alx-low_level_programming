39a99224efbec9744be0acbb19d431baa51bdad8
Abiola Ajibowu <talk2sherifabiola@yahoo.com>
2022-04-17 02:57:34 -0700

    Use unistd.io lobrary to print'

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - Prints out a string to standard error
@@ -7,6 +8,6 @@
  */
 int main(void)
 {
-putchar("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 return (1);
 }
