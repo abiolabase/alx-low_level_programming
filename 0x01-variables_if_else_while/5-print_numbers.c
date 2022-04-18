9cb885713373d99639a3c8232f1a7c145f2dc1e1
Abiola Ajibowu <talk2sherifabiola@yahoo.com>
2022-04-18 02:23:42 -0700

    Print 0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabet to display
+ *
+ * Return: 0
+ */
+int main(void)
+{
+int c;
+c = 0;
+while (c <= 9)
+{
+printf("%d",c);
+c++;
+}
+printf("\n");
+return (0);
+}
