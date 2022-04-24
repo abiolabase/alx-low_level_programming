3ecf1497863a3dad8c8e5f9ed0e46087fdcf2a93
Abiola Ajibowu <talk2sherifabiola@yahoo.com>
2022-04-24 06:22:56 -0700

    Swap a to b

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
- * main - swaps the value of a with b and vice verca
+ * swap_int - swaps the value of a with b and vice verca
  * @a: first parameter to swap
  * @b: second parameter to swap
  *
  * Return: swapped values
  */
-int swap_int(int *a, int *b)
+void swap_int(int *a, int *b)
 {
 int c;
-c = &a;
-a = &b;
-b = c;
-return (0);
+c = *a;
+*a = *b;
+*b = c;
 }
