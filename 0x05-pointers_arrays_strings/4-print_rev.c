b8cbf92a348f419b190c4fdcc2fa8b1213da0fa2
Abiola Ajibowu <talk2sherifabiola@yahoo.com>
2022-04-24 07:05:59 -0700

    Print string in reverse order

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _print_rev - prints string in reverse to stdout
+ * @s: string to print
+ *
+ * Return: Void
+ */
+void _print_rev(char *s)
+{
+char y[strlen(s)];
+int i;
+for (i = 0; i < strlen(s); i++)
+{
+y[i] = s[strlen(s) - i];
+}
+printf("%s\n", y);
+}
