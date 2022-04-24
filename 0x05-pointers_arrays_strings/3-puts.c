d64c6b0dc99adb7e2857dc19053f5cd75edda65e
Abiola Ajibowu <talk2sherifabiola@yahoo.com>
2022-04-24 06:50:29 -0700

    Print string to output

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts - prints string to stdout
+ * @s: string to print
+ *
+ * Return: Void
+ */
+void _puts(char *s)
+{
+printf("%s\n",s);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,3 +1,5 @@
 void swap_int(int *a, int *b);
 
 int _strlen(char *s);
+
+void _puts(char *s);
