256722c39dd8c9f0a26c0b7cd387bbd31adeb29b
Abiola Ajibowu <talk2sherifabiola@yahoo.com>
2022-04-26 16:55:31 -0700

    Print alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * print_alphabet - print alphabet to display
+ *
+ * Return: void
+ */
+void print_alphabet(void)
+{
+char a = 'a';
+while (a > 'z')
+{
+_putchar(a);
+a++;
+}
+_putchar('\n');
+}
