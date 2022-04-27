5442514165aa4bd087b4e4b8f4ee46f524b7037c
Abiola Ajibowu <talk2sherifabiola@yahoo.com>
2022-04-26 17:48:23 -0700

    Is alphabet lower or not

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet - print alphabet to display
+ *
+ * Return: 1 or 0
+ */
+int islower(int c)
+{
+if (c >= 141 && c <= 172)
+{
+Return (1);
+}
+else
+{
+Return (0);
+}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -7,4 +7,10 @@
 
 int _putchar(char c);
 
+int _islower(int c);
+
+void print_alphabet_x10(void);
+
+void print_alphabet(void);
+
 #endif
