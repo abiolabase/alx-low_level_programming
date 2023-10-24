3b5a965ac4072dea8a98533dcedcaa1102cb2b06
Abiola Ajibowu <talk2sherifabiola@yahoo.com>
2023-10-23 19:38:17 -0700

    Fix blank line

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -6,10 +6,10 @@
  * Return: 0 if s1 and s2 are equals,
  * another number if not.
  */
-
 int _strcmp(char *s1, char *s2)
 {
 int i = 0, op = 0;
+
 while (op == 0)
 {
 if ((*(s1 + i) == '\0') && (*(s2 + i) == '\0'))
