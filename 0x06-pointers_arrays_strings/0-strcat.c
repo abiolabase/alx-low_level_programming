5851e6803a1f2084fed4a1cbf859b0ae15f9d84d
Abiola Ajibowu <talk2sherifabiola@yahoo.com>
2023-10-24 00:43:32 -0700

    Replace header.h with main.h

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,4 +1,4 @@
-#include "header.h"
+#include "main.h"
 /**
  * _strcat - concatenates two strings,
  * @dest: destination.
--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,4 +1,4 @@
-#include "header.h"
+#include "main.h"
 /**
  * _strcmp - compares two strings
  * @s1: first string.
