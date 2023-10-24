94b04f26e40c49294f1ed124959746c43b6ab3ac
Abiola Ajibowu <talk2sherifabiola@yahoo.com>
2023-10-23 20:39:34 -0700

    Update with Betty

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,13 +1,14 @@
 #include "header.h"
 /**
- *  * leet - encodes a string into 1337
- *   * @s: input string.
- *    * Return: the pointer to dest.
- *     */
+ * leet - encodes a string into 1337
+ * @s: input string.
+ * Return: the pointer to dest.
+ */
 
 char *leet(char *s)
 {
 	int count = 0, i;
+
 	int low_letters[] = {97, 101, 111, 116, 108};
 	int upp_letters[] = {65, 69, 79, 84, 76};
 	int numbers[] = {52, 51, 48, 55, 49};
